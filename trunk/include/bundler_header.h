f83f9303a3c25e36f8669deffc0f7490eb7b329a
garrynewman@gmail.com <garrynewman@gmail.com@042f7add-113b-a4db-522e-1a4349ab60a9>
2012-02-11 18:03:32 +0000

    Bundler fully functional

--- trunk/include/bundler_header.h
+++ trunk/include/bundler_header.h
@@ -4,7 +4,8 @@ const char BundlerIdent[6] = { 'B', 'U', 'N', 'D', '0', '1' };
 
 struct BundlerHeader
 {
-	char	ident[6];
-	int		launchersize;
-	int		zipsize;
+	char		ident[6];
+	int			launchersize;
+	int			zipsize;
+	char		command[256];
 };
--- trunk/src/bundler/bundler.cpp
+++ trunk/src/bundler/bundler.cpp
@@ -1,14 +1,19 @@
 #include "Bootil/Bootil.h"
+#include "bundler_header.h"
 
 using namespace Bootil;
 
-bool BuildFromFolder( const BString& strRootFolder, Compression::Zip::File& zip )
+bool BuildFromFolder( const BString& strRootFolder, Compression::Zip::File& zip, BString& strLauncher )
 {
 	String::List files;
 	File::GetFilesInFolder( strRootFolder, files, true );
 
 	BOOTIL_FOREACH( f, files, String::List )
 	{
+
+		if ( strLauncher.empty() && String::EndsWith( *f, ".exe" ) )
+			strLauncher = *f;
+
 		if ( !zip.AddFile( *f, strRootFolder + "/" + *f ) )
 		{
 			Output::Msg( "Couldn't add file %s\n", (strRootFolder + "/" + *f).c_str() );
@@ -29,6 +34,9 @@ int main( int argc, char *argv[] )
 	}
 
 	Compression::Zip::File	ZipFile;
+	BString					EmbeddedExe;
+	BString					OutputFilename;
+
 	BString strFolderOrZip = CommandLine::GetArg( 0, "" );
 
 	//
@@ -36,14 +44,70 @@ int main( int argc, char *argv[] )
 	//
 	if ( File::IsFolder( strFolderOrZip ) )
 	{
-		if ( !BuildFromFolder( strFolderOrZip, ZipFile ) )
+		if ( !BuildFromFolder( strFolderOrZip, ZipFile, EmbeddedExe ) )
 		{
 			Output::Msg( "Error with folder\n" );
 			return 0;
 		}
+
+		OutputFilename = strFolderOrZip + ".exe";
+	}
+	else
+	{
+		// TODO: Handle zip file.
+		return 0;
 	}
 
-	ZipFile.Write( strFolderOrZip + ".zip" );
+	//
+	// Did we find an executable file in the folder?
+	//
+	if ( EmbeddedExe.empty() )
+	{
+		Output::Msg( "No executable file found!\n" );
+		return 0;
+	}
 
-	Debug::PopupMessage( "HUUUH: %s", strFolderOrZip.c_str() );
+	//
+	// Write the zip file to a buffer
+	//
+	AutoBuffer ZipBuffer;
+	ZipFile.WriteAndClose( ZipBuffer );
+
+	//
+	// Load launcher.exe into a buffer
+	//
+	AutoBuffer Launcher;
+	if ( !File::Read( Platform::ProgramFolder() + "/launcher.exe", Launcher ) )
+	{
+		Output::Msg( "Couldn't find launcher.exe\n" );
+		return 0;
+	}
+
+	//
+	// Create the BundleHeader
+	//
+	BundlerHeader header;
+		memcpy( header.ident, BundlerIdent, sizeof(BundlerIdent) );
+		header.launchersize = Launcher.GetWritten();
+		header.zipsize = ZipBuffer.GetWritten();
+		memset( header.command, 0, sizeof(header.command) );
+		memcpy( header.command, EmbeddedExe.c_str(), EmbeddedExe.length() );
+
+	//
+	// Append the zip file and the head to the end of the launcher
+	//
+	Launcher.WriteBuffer( ZipBuffer );
+	Launcher.Write( &header, sizeof(header) );	
+
+	//
+	// Save the launcher as OutputFilename
+	//
+	File::Write( OutputFilename, Launcher );
+
+	//
+	// Report success
+	//
+	Output::Msg( "Outputted: %s (%s)\n", OutputFilename.c_str(), String::Format::Memory( Launcher.GetWritten() ).c_str() );
+
+	return 0;
 }
